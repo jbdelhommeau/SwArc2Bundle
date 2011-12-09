16055b64c2c0565c152202cc32ce7f0c7c3eb66d
Thibaut Cuvelier <dourouc05@gmail.com>
2011-12-09 22:49:42 +0100

    First commit: bundle creation

---
+++ Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sw\Arc2Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('SwArc2Bundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ SwArc2Bundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sw\Arc2Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SwArc2Bundle extends Bundle
+{
+}
