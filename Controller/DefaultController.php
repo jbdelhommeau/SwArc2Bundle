e906b8f8b73e9191c8dd1548076b18913f0b56ad
Thibaut Cuvelier <dourouc05@gmail.com>
2011-12-10 19:17:45 +0100

    Add a SPARQL endpoint.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,10 +4,8 @@ namespace Sw\Arc2Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 class DefaultController extends Controller
 {
-    
     public function indexAction($name)
     {
         return $this->render('SwArc2Bundle:Default:index.html.twig', array('name' => $name));
---
+++ Controller/SparqlEndpointController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Sw\Arc2Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+// TO DO: don't hardcode paths
+require(__DIR__ . '/../../../../arc2/ARC2.php');
+
+/**
+ * A SPARQL endpoint. 
+ *
+ * @author Thibaut
+ */
+class SparqlEndpointController
+{
+    public function indexAction()
+    {
+        // TO DO: use a semantic configuration for the bundle
+        $arc_config = array(
+            /* MySQL database settings */
+            'db_host' => 'localhost',
+            'db_user' => 'root',
+            'db_pwd' => '',
+            'db_name' => 'symfony',
+
+            /* ARC2 store settings */
+            'store_name' => 'sandbox',
+
+            /* SPARQL endpoint settings */
+            'endpoint_features' => array(
+                'select', 'construct', 'ask', 'describe', // allow read
+                'load', 'insert', 'delete',               // allow update
+                'dump'                                    // allow backup
+            ),
+            'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+            'endpoint_read_key' => '', /* optional */
+            'endpoint_write_key' => '', /* optional */
+            'endpoint_max_limit' => 250, /* optional */
+            );
+        
+        $ep = \ARC2::getStoreEndpoint($arc_config);
+
+        if (!$ep->isSetUp())
+        {
+            $ep->setUp(); /* create MySQL tables */
+        }
+        
+        $ep->go();
+    }
+}
