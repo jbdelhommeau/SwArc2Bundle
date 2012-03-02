3d6900320465afab5c43fc9f759e47b2a2563ac2
Thibaut Cuvelier <dourouc05@gmail.com>
2012-03-02 21:26:45 +0100

    Don't forget to add your files to the commit...

---
+++ Arc2/Arc2.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sw\Arc2Bundle\Arc2; 
+
+/**
+ * Description of SparqlEndpoint
+ *
+ * @author Thibaut
+ */
+class Arc2
+{
+    /**
+     * Constructor, with the options specified.  
+     * 
+     * @param array $options
+     */
+    public function __construct($arc_path)
+    {
+        require($arc_path . '/ARC2.php'); 
+		// ARC2 takes care of all the necessary loading (no namespace for these files, but 
+		// they autoload when necessary). 
+	}
+}
